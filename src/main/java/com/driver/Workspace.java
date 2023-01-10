d6129df1ccdbf15d575e7b8d0a836b6eec006549
RAHUL MALAWALIYA <103227604+RahulMalawaliya@users.noreply.github.com>
2023-01-10 15:48:28 +0530

    Update Workspace.java

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,21 +5,21 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
-    private ArrayList<Meeting> calendar; // Stores all the meetings
+    private ArrayList<Meeting> calendar = new ArrayList<>(); // Stores all the meetings
 
     public Workspace(String emailId) {
-        super(emailId,Integer.MAX_VALUE);
+        super(emailId, Integer.MAX_VALUE);
         // The inboxCapacity is equal to the maximum value an integer can store.
-        calendar = new ArrayList<Meeting>();
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-        calendar.add(meeting);
+        this.calendar.add(meeting);
 
     }
 
@@ -28,25 +28,25 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        Collections.sort(calendar,
-                (a,b) -> {
-                    return a.getStartTime().compareTo(b.getStartTime());
-                });
+        // m1 -> {10,11} , m2 -> {10:30, 11}, m3 -> {11,11:30}, m4 -> {11:15, 1145}
 
-        int count = 0;
+        ArrayList<Integer> m = new ArrayList<>();
 
-        LocalTime meetEnd = LocalTime.parse("00:00");
+        Meeting.mycomparator mc = new Meeting.mycomparator();
 
-        for(Meeting m : calendar){
-            LocalTime start = m.getStartTime();
-            LocalTime end = m.getEndTime();
+        // Sorting of meeting according to
+        // their finish time.
+        this.calendar.sort(mc);
 
-            if(meetEnd.compareTo(start)<0){
-                meetEnd=end;
-                count++;
+        m.add(this.calendar.get(0).getPos());
+        LocalTime time_limit = this.calendar.get(0).getEndTime();
+
+        for (int i = 1; i < calendar.size(); i++) {
+            if(this.calendar.get(i).getStartTime().isAfter(time_limit)) {
+                m.add(this.calendar.get(i).getPos());
+                time_limit = this.calendar.get(i).getEndTime();
             }
         }
-        return count;
-
+        return m.size();
     }
 }
